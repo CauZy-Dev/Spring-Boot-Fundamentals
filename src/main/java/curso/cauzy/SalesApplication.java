e8693942ee436b760fadbe6f83a105a3ba413e81
Cauã Farias <cauafariasdev@gmail.com>
2024-05-10 11:03:50 -0300

    feat: add restController from spring-boot-starter-web

--- src/main/java/curso/cauzy/SalesApplication.java
+++ src/main/java/curso/cauzy/SalesApplication.java
@@ -2,9 +2,17 @@ package curso.cauzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication // spring-boot-starter
+@RestController //spring-boot-starter-web
 public class SalesApplication {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "hello world";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SalesApplication.class, args);
