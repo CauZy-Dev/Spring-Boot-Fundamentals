e69beea6c1989f5f55dd25e2282223e18ab08d51
CauZy-Goes <cauafariasdev@gmail.com>
2024-05-29 12:02:38 -0300

    feat: create outro perfis de propeties

--- src/main/java/curso/cauzy/MinhaConfiguration.java
+++ src/main/java/curso/cauzy/MinhaConfiguration.java
@@ -1,10 +1,20 @@
 package curso.cauzy;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development") // so roda nesse ambiente;/ perfil
 public class MinhaConfiguration {
+
+    @Bean
+    public CommandLineRunner executar() { // executa assim que a aplicação inicializa
+        return args -> {
+            System.out.println("Executando a configuração de desenvolvimennto");
+        };
+    }
 
     // foi substituido pelo application.properties
 //    @Bean(name = "applicationName")  //crie esse objeto no contexto da aplicação, coneção com banco de dados
